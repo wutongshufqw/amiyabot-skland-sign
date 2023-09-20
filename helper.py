0561f05224bdfcab5af5e953ed95e59ba4df7b6d
wutongshufqw <fengqiwu2003@foxmail.com>
2023-09-20 23:16:32 +0800

    使用cred代替token

--- helper.py
+++ helper.py
@@ -1,19 +1,16 @@
+try:
+    import ujson as json
+except ImportError:
+    import json
+from pathlib import Path
 from typing import Tuple
 
 import requests
 
 app_code = '4ca99fa6b56cc2ba'
-header = {
-    'cred': '',
-    'User-Agent': 'Skland/1.0.1 (com.hypergryph.skland; build:100001014; Android 31; ) Okhttp/4.11.0',
-    'Accept-Encoding': 'gzip',
-    'Connection': 'close'
-}
-header_login = {
-    'User-Agent': 'Skland/1.0.1 (com.hypergryph.skland; build:100001014; Android 31; ) Okhttp/4.11.0',
-    'Accept-Encoding': 'gzip',
-    'Connection': 'close'
-}
+curr_path = Path(__file__).parent
+header_path = curr_path / 'header.json'
+header_login_path = curr_path / 'header_login.json'
 # 使用token获得认证代码
 grant_code_url = "https://as.hypergryph.com/user/oauth2/v2/grant"
 # 使用认证代码获得cred
@@ -28,91 +25,69 @@ class ArknightsException(Exception):
     pass
 
 
-def get_cred(grant_code: str):
-    resp = requests.post(
-        cred_code_url,
-        json={
-            'code': grant_code,
-            'kind': 1
-        },
-        headers=header_login
-    ).json()
-    if resp['code'] != 0:
-        raise ArknightsException(f'获得cred失败：{resp["message"]}')
-    return resp['data']['cred']
-
-
-def get_binding_list() -> list:
-    v = []
-    resp = requests.get(binding_url, headers=header).json()
-    if resp['code'] != 0:
-        msg = f"请求角色列表出现问题: {resp['message']}"
-        if resp.get('message') == '用户未登录':
-            msg += f'用户登录可能失效了, 请重新运行此程序!'
-        raise ArknightsException(msg)
-    for i in resp['data']['list']:
-        if i.get('appCode') != 'arknights':
-            continue
-        v.extend(i.get('bindingList'))
-    return v
-
-
-def do_sign(cred: str) -> str:
-    header['cred'] = cred
-    characters = get_binding_list()
-    msgs = []
-    for i in characters:
-        body = {
-            'uid': i.get('uid'),
-            'gameId': 1
-        }
-        resp = requests.post(sign_url, headers=header, json=body).json()
-        if resp['code'] != 0:
-            msgs.append(f'角色{i.get("nickName")}({i.get("channelName")})签到失败了!原因: {resp.get("message")}')
-            continue
-        awards = resp['data']['awards']
-        for j in awards:
-            res = j['resource']
-            msgs.append(
-                f'角色{i.get("nickName")}({i.get("channelName")})签到成功, 获得了{res["name"]}×{j.get("count") or 1}'
-            )
-    return '\n\n'.join(msgs)
-
-
 class ArknightsHelper:
-    def __init__(self, token: str):
-        self.token = token
+    def __init__(self, cred: str):
+        self.cred = cred
+
+    @property
+    def header_login(self) -> dict:
+        with open(header_login_path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
+    @property
+    def header(self) -> dict:
+        with open(header_path, 'r', encoding='utf-8') as f:
+            header = json.load(f)
+            header['cred'] = self.cred
+            return header
+
+    def get_binding_list(self) -> list:
+        v = []
+        resp = requests.get(binding_url, headers=self.header).json()
+        if resp['code'] != 0:
+            msg = f"请求角色列表出现问题: {resp['message']}"
+            if resp.get('message') == '用户未登录':
+                msg += '\n用户登录可能失效了, 请重新运行此程序!'
+            raise ArknightsException(msg)
+        for i in resp['data']['list']:
+            if i.get('appCode') != 'arknights':
+                continue
+            v.extend(i.get('bindingList'))
+        return v
+
+    def do_sign(self) -> str:
+        characters = self.get_binding_list()
+        msgs = []
+        for i in characters:
+            body = {
+                'uid': i.get('uid'),
+                'gameId': 1
+            }
+            resp = requests.post(sign_url, headers=self.header, json=body).json()
+            if resp['code'] != 0:
+                msgs.append(f'角色{i.get("nickName")}({i.get("channelName")})签到失败了!原因: {resp.get("message")}')
+                continue
+            awards = resp['data']['awards']
+            for j in awards:
+                res = j['resource']
+                msgs.append(
+                    f'角色{i.get("nickName")}({i.get("channelName")})签到成功, 获得了{res["name"]}×{j.get("count") or 1}'
+                )
+        return '\n\n'.join(msgs)
 
     async def sign(self) -> Tuple[str, str]:
         """签到
         :return: msg: 消息, info: 状态 (success, warning, error)
         """
         try:
-            msgs = do_sign(self.get_cred_by_token())
+            msgs = self.do_sign()
             return msgs, 'success'
         except ArknightsException as e:
-            return f'签到失败了, 请检查token是否正确 -> 发送 [兔兔绑定] 查看使用说明, 失败原因: {e.__str__()}', 'error'
+            return f'签到失败了, 请检查cred是否正确 -> 发送 [兔兔设置凭证] 查看使用说明, 失败原因: {e.__str__()}', 'error'
         except Exception as e:
             return f'签到失败了, 原因: {e}', 'error'
 
-    def get_cred_by_token(self) -> str:
-        grant_code = self.get_grant_code()
-        return get_cred(grant_code)
-
-    def get_grant_code(self) -> str:
-        response = requests.post(
-            grant_code_url,
-            json={
-                'appCode': app_code,
-                'token': self.token,
-                'type': 0
-            },
-            headers=header_login
-        )
-        resp = response.json()
-        if response.status_code != 200:
-            raise ArknightsException(f'获得认证代码失败: {resp}')
-        if resp.get('status') != 0:
-            raise ArknightsException(f'获得认证代码失败: {resp["msg"]}')
-        return resp['data']['code']
-
+    @staticmethod
+    def tip() -> bytes:
+        pic_path = curr_path / 'tip.png'
+        return pic_path.read_bytes()
--- main.py
+++ main.py
@@ -29,18 +29,17 @@ class SklandSign(SignBaseModel):
     remark: str = CharField(null=True)
 
     @staticmethod
-    def get_skland_sign(user_id: str) -> bool:
-        result = SklandSign.get_or_none(SklandSign.user_id == user_id)
-        if result:
-            return result.open
-        else:
-            return False
+    def get_skland_sign(user_id: str) -> Optional['SklandSign']:
+        return SklandSign.get_or_none(SklandSign.user_id == user_id)
 
     @staticmethod
-    def set_skland_sign(user_id: str, open_: bool):
+    def set_skland_sign(user_id: str, open_: Optional[bool] = None, cred: Optional[str] = None):
         result = SklandSign.get_or_none(SklandSign.user_id == user_id)
         if result:
-            result.open = open_
+            if open_:
+                result.open = open_
+            if cred:
+                result.remark = cred
             return result.save()
         else:
             return SklandSign.create(user_id=user_id, open=open_)
@@ -56,7 +55,7 @@ class ToolsPluginInstance(AmiyaBotPluginInstance):
 
 bot = ToolsPluginInstance(
     name='森空岛签到插件',
-    version='1.0',
+    version='1.1',
     plugin_id='amiyabot-skland-sign',
     plugin_type='tools',
     description='森空岛每日签到',
@@ -112,6 +111,19 @@ async def group_send_message(group: str, msg: str, markdown: bool = True):
                 await main_bot[sp[0]].send_message(Chain().text(msg), channel_id=sp[1])
 
 
+@bot.on_message(keywords=['设置凭证'], allow_direct=True, level=5)
+async def set_skland(data: Message):
+    res = await data.wait(Chain(data).image(ArknightsHelper.tip()).text(
+        '森空岛网址: \nhttps://www.skland.com/\n方法二代码:\nlocalStorage.getItem("SK_OAUTH_CRED_KEY");\n方法三代码:\njavascript:prompt(undefined, localStorage.getItem("SK_OAUTH_CRED_KEY"));'),
+        max_time=120)
+    if res:
+        await res.recall()
+        cred = res.text_original
+        SklandSign.set_skland_sign(data.user_id, cred=cred)
+        return Chain(data).text('设置成功')
+    return Chain(data).text('操作超时')
+
+
 @bot.on_message(keywords=['方舟签到'], allow_direct=True, level=5)
 async def sign_skland(data: Message):
     async def true_or_false(message: Message):
@@ -119,59 +131,46 @@ async def sign_skland(data: Message):
             return True
 
     user_id = data.user_id
-    if 'amiyabot-skland' in main_bot.plugins:
-        plugin = main_bot.plugins['amiyabot-skland']
-        token: Optional[str] = await plugin.get_token(user_id)
-        if token:
-            if SklandSign.get_skland_sign(user_id):
-                reply = await data.wait(Chain(data).text('森空岛签到已开启，是否关闭？(是/否)'), data_filter=true_or_false, max_time=20)
-                if reply:
-                    if reply.text_original == '是':
-                        SklandSign.set_skland_sign(user_id, False)
-                        return Chain(data).text('森空岛签到已关闭')
-                    else:
-                        reply = await data.wait(Chain(data).text('是否立即签到？(是/否)'), data_filter=true_or_false, max_time=20)
-                        if reply:
-                            if reply.text_original == '是':
-                                msg, _ = await ArknightsHelper(token).sign()
-                                return Chain(data).text(msg)
-                return Chain(data).text('操作已取消')
-            else:
-                reply = await data.wait(Chain(data).text('森空岛签到已关闭，是否开启？(是/否)'), data_filter=true_or_false, max_time=20)
-                if reply:
-                    if reply.text_original == '是':
-                        SklandSign.set_skland_sign(user_id, True)
-                        reply = await data.wait(Chain(data).text('森空岛签到已开启，是否立即签到？(是/否)'), data_filter=true_or_false, max_time=20)
-                        if reply:
-                            if reply.text_original == '是':
-                                msg, info = await ArknightsHelper(token).sign()
-                                return Chain(data).text(msg)
-                return Chain(data).text('操作已取消')
-        else:
-            return Chain(data).text('博士，您尚未绑定 Token，请发送 “兔兔绑定” 查看绑定说明。')
-    else:
-        return Chain(data).text('兔兔尚未安装森空岛插件，请联系管理员安装插件。')
+    sign = SklandSign.get_skland_sign(user_id)
+    if sign and sign.remark:
+        if sign.open:
+            reply = await data.wait(Chain(data).text('森空岛签到已开启，是否关闭？(是/否)'), data_filter=true_or_false, max_time=20)
+            if reply:
+                if reply.text_original == '是':
+                    SklandSign.set_skland_sign(user_id, open_=False)
+                    return Chain(data).text('森空岛签到已关闭')
+                reply = await data.wait(Chain(data).text('是否立即签到？(是/否)'), data_filter=true_or_false, max_time=20)
+                if reply and reply.text_original == '是':
+                    msg, _ = await ArknightsHelper(sign.remark).sign()
+                    return Chain(data).text(msg)
+            return Chain(data).text('操作已取消')
+        reply = await data.wait(Chain(data).text('森空岛签到已关闭，是否开启？(是/否)'), data_filter=true_or_false, max_time=20)
+        if reply and reply.text_original == '是':
+            SklandSign.set_skland_sign(user_id, open_=True)
+            reply = await data.wait(Chain(data).text('森空岛签到已开启，是否立即签到？(是/否)'), data_filter=true_or_false, max_time=20)
+            if reply and reply.text_original == '是':
+                msg, _ = await ArknightsHelper(sign.remark).sign()
+                return Chain(data).text(msg)
+        return Chain(data).text('操作已取消')
+    return Chain(data).text('博士，您尚未绑定 Cred，请发送 “兔兔设置凭证” 查看绑定说明。')
 
 
 @bot.timed_task(sub_tag='skland_sign', trigger='cron', hour=6)
 async def skland_sign(instance: BotHandlerFactory):
-    if 'amiyabot-skland' in main_bot.plugins:
-        msgs = []
-        plugin = main_bot.plugins['amiyabot-skland']
-        users = SklandSign.get_skland_sign_open()
-        for user in users:
-            user_id = user.user_id
-            token: Optional[str] = await plugin.get_token(user_id)
-            if token:
-                if SklandSign.get_skland_sign(user_id):
-                    msg, _ = await ArknightsHelper(token).sign()
-                    msgs.append(f'用户 {user_id} 签到结果: {msg}')
-            else:
-                msgs.append(f'用户 {user_id} 未绑定 Token')
-        if msgs:
-            config = bot.get_config('skland_sign')
-            if config:
-                groups = []
-                for group in config:
-                    groups.append(f'{group["appid"]}_{group["group"]}')
-                await groups_send_message(groups, '\n\n'.join(msgs))
+    msgs = []
+    users = SklandSign.get_skland_sign_open()
+    for user in users:
+        user_id = user.user_id
+        cred = user.remark
+        if cred:
+            msg, _ = await ArknightsHelper(cred).sign()
+            msgs.append(f'用户 {user_id} 签到结果: {msg}')
+        else:
+            msgs.append(f'用户 {user_id} 未绑定 Cred')
+    if msgs:
+        config = bot.get_config('skland_sign')
+        if config:
+            groups = []
+            for group in config:
+                groups.append(f'{group["appid"]}_{group["group"]}')
+            await groups_send_message(groups, '\n\n'.join(msgs))
